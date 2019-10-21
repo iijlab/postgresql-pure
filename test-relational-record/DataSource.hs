61dd4fb07648945d8c3cbe3236d2e38c8b7561ab
Kazuki Okamoto <kazukiokamoto@iij.ad.jp>
2019-10-21 16:11:04 +0900

    for Linux

--- test-relational-record/DataSource.hs
+++ test-relational-record/DataSource.hs
@@ -4,4 +4,4 @@ import           Data.Default.Class       (def)
 import           Database.HDBC.PostgreSQL (Connection, connectPostgreSQL)
 
 connect :: IO Connection
-connect = connectPostgreSQL "user='postgres'"
+connect = connectPostgreSQL "host='localhost' user='postgres'"
