f8013ac8e960d6a43c474db03eb5424dfb57a985
Kazuki Okamoto <kazukiokamoto@iij.ad.jp>
2019-11-01 17:38:37 +0900

    add build task to psake

--- psakefile.ps1
+++ psakefile.ps1
@@ -1,3 +1,27 @@
+# call like Invoke-psake -Task Build -parameters @{'ghc' = 'all'; 'test' = $true}
+Task Build {
+    if ($ghc -eq 'all') { $ghc = @('8.4', '8.6') }
+    else { if ($ghc -eq $null) { $ghc = @('8.6') } }
+    foreach ($c in $ghc) {
+        switch ($c) {
+            '8.4' { $stack_args = @('--stack-yaml', 'stack-ghc-8.4.yaml') }
+            '8.6' { $stack_args = @('--stack-yaml', 'stack-ghc-8.6.yaml') }
+            'nightly' { $stack_args = @('--stack-yaml', 'stack-ghc-8.6.yaml', '--resolver', 'nightly') }
+            default { Write-Error "Unexpected GHC: $c"}
+        }
+        if ($test -eq $null) {
+            Exec { stack $stack_args build --ghc-options -Werror }
+        }
+        else {
+            if ($test.GetType() -eq [string] -and $test -eq 'all') { $test = @('doctest', 'hdbc-postgresql', 'original', 'relational-record') }
+            else { if ($test.GetType() -eq [boolean] -and $test -eq $true) { $test = @('doctest', 'hdbc-postgresql', 'original') } }
+            foreach ($t in $test) {
+                Exec { stack $stack_args test --ghc-options -Werror postgresql-pure:test:$t }
+            }
+        }
+    }
+}
+
 Task Format {
     Exec { Get-ChildItem -Filter '*.hs' -Recurse src, test, test-doctest, test-relational-record, benchmark | Where-Object { $_.Directory -notlike '*\src\Database\PostgreSQL\Simple\Time\Internal' } | ForEach-Object { stack exec -- stylish-haskell -i $_.FullName } }
 }
